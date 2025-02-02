56c317c2e7366248a5602d13725e2cd2b1f80954
The 86 Gang <theeightysixgang@gmail.com>
2025-02-02 22:43:48 +0100

    todos ven los coches

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function loadCars() {
         const cars = JSON.parse(localStorage.getItem('cars')) || [];
         cars.forEach(car => {
-            addCarToGallery(car.name, car.image, car.description, car.userId);
+            addCarToGallery(car.name, car.image, car.description, car.userId, car.id);
         });
     }
 
@@ -25,16 +25,18 @@ document.addEventListener('DOMContentLoaded', function () {
             const carImage = carItem.querySelector('img').src;
             const carDescription = carItem.querySelector('p').textContent;
             const carUserId = carItem.getAttribute('data-user-id');
-            cars.push({ name: carName, image: carImage, description: carDescription, userId: carUserId });
+            const carId = carItem.getAttribute('data-id');
+            cars.push({ name: carName, image: carImage, description: carDescription, userId: carUserId, id: carId });
         });
         localStorage.setItem('cars', JSON.stringify(cars));
     }
 
     // Función para agregar un coche a la galería
-    function addCarToGallery(name, image, description, carUserId) {
+    function addCarToGallery(name, image, description, carUserId, carId) {
         const newCar = document.createElement('div');
         newCar.classList.add('car-item');
         newCar.setAttribute('data-user-id', carUserId);
+        newCar.setAttribute('data-id', carId);
 
         const carImageElement = document.createElement('img');
         carImageElement.src = image;
@@ -68,11 +70,12 @@ document.addEventListener('DOMContentLoaded', function () {
             const carName = document.getElementById('car-name').value;
             const carImage = document.getElementById('car-image').files[0];
             const carDescription = document.getElementById('car-description').value;
+            const carId = 'car-' + Date.now(); // Generar un identificador único para cada coche
 
             const reader = new FileReader();
             reader.onload = function (e) {
                 const carImageSrc = e.target.result;
-                addCarToGallery(carName, carImageSrc, carDescription, userId);
+                addCarToGallery(carName, carImageSrc, carDescription, userId, carId);
                 saveCars();
                 form.reset();
             };
